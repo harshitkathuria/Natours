d6446dc6854e34fa3bf6f885c5051f2e5092331f
harshitkathuria <harshitkathuria14@gmail.com>
2020-08-04 22:23:39 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(DB, {
 })
 
 //Start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Listening from port ${port}`);
 });
